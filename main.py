7b03884963a321234b59f2c88bdbd577e86cdbfb
Artur Demenskiy <artur.demenskiy@elrom.org>
2023-06-09 12:22:58 +0300

    initialize

---
+++ main.py
@@ -0,0 +1,52 @@
+# print something
+print(123)
+
+# decalre variables
+test = 5
+test2 = 'name'
+
+# types in python
+# int
+int = 5 
+# float
+#float = 5.4
+# str
+str = "namsdasd"
+# bool
+bool = False
+
+# Ecranation
+print("he\" is \"bas")
+
+# \n
+print("Hello \n world")
+
+# std in & out
+getName = input("Get in your Name: ")
+age = input("Get in your Age: ")
+
+print("age " + age + " name " + getName)
+
+# import math
+# a = 23123.33
+# print(math.floor(a))
+
+# calculator
+
+# what = input(" + or -")
+# a = float(input("get first number"))
+# b = float(input("get second number"))
+
+# if what == "+":
+#     c = a + b
+#     print("Result: " + str(c))
+# elif what == "-":
+#     c = a - b
+#     print("Result: " + str(c))
+# else:
+#     print("Wrong operation")
+
+from colorama import init
+from colorama import Back
+
+print(Back.GREEN)
